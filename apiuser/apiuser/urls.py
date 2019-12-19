61b792a6da1712566f5c7a81f7af63a9e157afb6
ngel rojas p <aomarrojasp@gmail.com>
2019-12-18 21:55:45 -0300

    updated more

--- apiuser/apiuser/urls.py
+++ apiuser/apiuser/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
@@ -32,4 +34,4 @@ urlpatterns = [
     path('api/v1/', include('newscampaing.urls')),
     path('api/v1/', include('comment.urls')),
     path('api/v1/', include('currency.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
